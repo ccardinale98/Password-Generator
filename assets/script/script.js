363cabc5bfaa5ebbcf2989312c977178ab13dbf3
ccardinale98 <ccardinale98@gmail.com>
2021-08-06 10:50:04 -0400

    rewrote my program

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,51 +1,77 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var specChars = ['!', '#', '$', '%', '&', '*', '+', '-', ':', ';', '<', '=', '>', '?', '@', '^', '_', '~'];
+var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerLettersString = ('abcdefghijklmnopqrstuvwxyz');
+var lowerLetters = lowerLettersString.split('');
+var upperLettersString = lowerLettersString.toUpperCase();
+var upperLetters = upperLettersString.split('');
+//array variables: specChars, nums, lowerLetters, upperLetters
 
-// Write password to the #password input
-function writePassword() {
-  var passLength = prompt('Criteria \n\nPassword Length (number of characters): ');
-  // var passLower = confirm('Criteria \n\nInclude lower case letters?');
-  var passUpper = confirm('Criteria \n\nInclude upper case letters?');
-  var passNumber = confirm('Criteria \n\nInclude numbers?');
-  var passSpecial = confirm('Criteria \n\nInclude special characters?');
+function getUserOptions() {
+  var passLengthString = prompt('Criteria \n\nPassword Length (number of characters): ');
+  var passLength = parseInt(passLengthString);
 
-  var passLengthInt = parseInt(passLength);
-  var charPoss = [];
-  var numbPoss = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var specPoss = ['!', '#', '$', '%', '&', '*', '+', '-', ':', ';', '<', '=', '>', '?', '@', '^', '_', '~'];
-  const upper = Array.from(Array(26)).map((e, i) => i + 65);
-  const upperPoss = upper.map((x) => String.fromCharCode(x));
-  var lowerPoss = upperPoss.toLowerCase;
-
-  if (passUpper == true) {
-    charPoss = charPoss.concat(upperPoss);
-    charPoss = charPoss.concat(lowerPoss);
-  } else {
-    charPoss = charPoss.concat(lowerPoss);
+  //makes sure the password length input is a number, not letters
+  if (isNaN(passLength) == true) {
+    alert('Must input a number.');
+    return getUserOptions();
   }
 
-  if (passNumber == true) {
-    charPoss = charPoss.concat(numbPoss);
+  //makes sure the password length input is between 8 and 128 characters
+  if (passLength < 8 || passLength > 128) {
+    alert('Must be between 8 and 128 characters.');
+    return getUserOptions();
   }
 
-  if (passSpecial == true) {
-    charPoss = charPoss.concat(specPoss);
+  //variables storing info from the criteria confirm windows
+  var specialCharConfirm = confirm('Criteria \n\nInclude upper case letters?');
+  var numsConfirm = confirm('Criteria \n\nInclude numbers?');
+  var upperLettersConfirm = confirm('Criteria \n\nInclude upper case letters?');
+  var lowerLettersConfirm = confirm('Criteria \n\nInclude lower case letters?');
+
+  //single variable that stores all the user input from criteria confirm windows
+  var passOptions = {
+    passLengthOption: passLength,
+    specCharsOption: specialCharConfirm,
+    numsOption: numsConfirm,
+    upperLettersOption: upperLettersConfirm,
+    lowerLettersOption: lowerLettersConfirm,
   }
 
-  // function generatePassword() {
-    var password = Array(passLengthInt).fill(charPoss).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
-  // }
-  // var password = generatePassword();  
-  // generatePassword();
-  console.log(password);
+  return passOptions;
 }
 
 
 
-var passwordText = document.querySelector("#password");
+function generatePassword() {
+  var userOptions = getUserOptions();
+  var generatedPass = [];
+  var userPosChars = [];
+  var guarChar = [];
 
-passwordText.value = password;
+  if (userOptions.specCharsOption == true) {
+    userPosChars = userPosChars.concat(specChars);
+  }
+  if (userOptions.numsOption == true) {
+    userPosChars = userPosChars.concat(nums);
+  }
+  if (userOptions.upperLettersOption == true) {
+    userPosChars = userPosChars.concat(upperLetters);
+  }
+  if (userOptions.lowerLettersOption == true) {
+    userPosChars = userPosChars.concat(lowerLetters);
+  }
+  console.log(userPosChars)
+  for(i = 0; i < userOptions.passLengthOption; i++) {
+    generatedPass.push(userPosChars[Math.floor(Math.random() * (userPosChars.length - 1))])
 
-// Add event listener to generate button
-// This is saying when I click the button it will run the writePassword function
-generateBtn.addEventListener("click", writePassword);
+    console.log(generatedPass)
+  }
+
+  var password = generatedPass.join('')
+
+  console.log(password)
+}
+
+generatePassword()
+
+
